7dd9a18aacf65047b39fe901bdeb24c059ed5b58
matasdeveikis <matas.deveikis@gmail.com>
2020-12-09 09:30:16 +0000

    Create opentrons_MAGE.py
    Steps (WIP)

---
+++ opentrons_MAGE.py
@@ -0,0 +1,36 @@
+# Define reservoirs
+
+# Step: Amplification of SEVA CRISPR plasmid (Optional)
+# Modules: 
+# Inputs: Number of samples
+# Outputs: 
+
+# Step: Restriction digestion of SEVA CRISPR plasmids
+# Modules:
+# Inputs: Number of samples
+# Outputs:    
+
+# Step: Insertion of spacers into the SEVA CRISPR plasmid
+# Modules: 
+# Inputs: Number of samples, unique spacers
+# Outputs: 
+
+# Step: 
+# Modules:
+# Inputs: 
+# Outputs:
+    
+# Step: 
+# Modules:
+# Inputs: 
+# Outputs:
+    
+# Step: 
+# Modules:
+# Inputs: 
+# Outputs:
+    
+# Step: 
+# Modules:
+# Inputs: 
+# Outputs:
