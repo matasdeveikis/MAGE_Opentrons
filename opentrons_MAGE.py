32015df58fc3aa116ed97f409f54795ec8ee5bea
matasdeveikis <matas.deveikis@gmail.com>
2020-12-09 11:40:41 +0000

    steps

--- opentrons_MAGE.py
+++ opentrons_MAGE.py
@@ -5,32 +5,29 @@
 # Inputs: Number of samples
 # Outputs: 
 
-# Step: Restriction digestion of SEVA CRISPR plasmids
+# Step: Restriction digestion of SEVA CRISPR plasmids (Optional)
 # Modules:
-# Inputs: Number of samples
+# Inputs: Number of samples, lysed and resuspended SEVA CRISPR E. coli
 # Outputs:    
+# Comments: Use magnetic bead purification instead of gel electrophoresis
 
 # Step: Annealing of spacer oligonucleotides
-# Modules:
+# Modules: Thermocycler
 # Inputs: Number of samples, unique spacers
 # Outputs:
+# Comments: Ignore Step 1
 
 # Step: Insertion of spacers into the SEVA CRISPR plasmid
 # Modules: 
 # Inputs: Number of samples, annealed spacers
 # Outputs: 
     
-# Step: 
-# Modules:
-# Inputs: 
+# Step: ssDNA and CRISPR plasmid HEAT SHOCK (instead of electroporations)
+# Modules: Temperature
+# Inputs: electrocompetent cells with Ssr, stock solutions of the ssDNA mutagenic oligo, target temperature/other conditions as a user input
 # Outputs:
-    
-# Step: 
-# Modules:
-# Inputs: 
-# Outputs:
-    
-# Step: 
+
+# Step: Dilution for curation of plasmids (Optional)
 # Modules:
 # Inputs: 
 # Outputs:
