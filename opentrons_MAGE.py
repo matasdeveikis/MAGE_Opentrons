f2b9261dd39f15427382f2265d6316b55eeec242
matasdeveikis <matas.deveikis@gmail.com>
2020-12-15 00:20:17 +0000

    user input version

--- opentrons_MAGE.py
+++ opentrons_MAGE.py
@@ -1,55 +1,52 @@
-#Import opentrons and run
-#import sys
-#!{sys.executable} -m pip install opentrons
+# import opentrons and run
+# import sys
+# !{sys.executable} -m pip install opentrons
 
-import numpy as np
 import math
 from opentrons import simulate
 metadata = {'apiLevel': '2.8'}
 protocol = simulate.get_protocol_api('2.8')
 
-#Labware
-# Assume that we start with a P. putida strain that has edd deleted and posseses both Cas9 and recombinase plasmids
 
-tiprack_20 = [
-        protocol.load_labware(
-            'opentrons_96_filtertiprack_20ul', str(s), '300ul Tips')
-        for s in [3]]
-
+# Labware
 tiprack_300 = [
         protocol.load_labware(
             'opentrons_96_tiprack_300ul', str(s), '300ul Tips')
         for s in [1, 2]]
+tiprack_20 = [
+        protocol.load_labware(
+            'opentrons_96_filtertiprack_20ul', str(s), '300ul Tips')
+        for s in [10, 3]]   # its better to use 95 oligos instead of 96 as the you don't need to have a tiprack in module 10 that way
+                            # as 
 
-temp_hot = protocol.load_module('tempdeck', 4)                #For Heatshock
-hot_plate = temp_hot.load_labware('corning_96_wellplate_360ul_flat')
-
-reservoir15 = protocol.load_labware('nest_12_reservoir_15ml', 5)      #Bacterial Culture(A1), LB_Media(A6) and PBS
-
-temp_cold = protocol.load_module('tempdeck', 6) #For Heatshock
-
+temp_cold = protocol.load_module('tempdeck', 4) # For Heatshock
 cold_plate = temp_cold.load_labware('corning_96_wellplate_360ul_flat')
 
-storage_oligos = protocol.load_labware('opentrons_96_aluminumblock_generic_pcr_strip_200ul', 7)  #Stores Oligos
+temp_hot = protocol.load_module('tempdeck', 6)                # For Heatshock
+hot_plate = temp_hot.load_labware('corning_96_wellplate_360ul_flat')
 
-tube2 = protocol.load_labware('opentrons_24_tuberack_generic_2ml_screwcap', 10)
+reservoir15 = protocol.load_labware('nest_12_reservoir_15ml', 5)      # Bacterial Culture(A1), LB_Media(A6) and PBS
+storage_oligos = protocol.load_labware('opentrons_96_aluminumblock_generic_pcr_strip_200ul', 7)  # Stores mutagenic oligos
+tube2 = protocol.load_labware('opentrons_24_tuberack_generic_2ml_screwcap', 8)
 
-#pipettes
+# Pipettes
 p20 = protocol.load_instrument('p20_single_gen2', 'left', tip_racks=tiprack_20)
 p300 = protocol.load_instrument('p300_multi_gen2', 'right', tip_racks=tiprack_300)
 protocol.max_speeds['Z'] = 10
 
 # Variables
-plasmid_conc = 20 * np.ones(1) # In ng/uL
-oligos = 96
-growth_temp = 37
+plasmid_conc = input("Input the CRISPR plasmid concentration in ug/mL: ")
+oligos = input("Input number of mutagenic oligos to be used: ")
+growth_temp = input("Input incubation temperature for the chosen bacteria: ")
 delay_per_column = 5 # in seconds, needs to be experimentally determined (5s is assumed for now)
 electroporation = False
 
-#Reagents
+# Reagents
+# Assume that we start with a P. putida strain that has edd deleted and posseses both Cas9 and recombinase plasmids
 Bacteria = reservoir15.wells ('A1')
 Media = reservoir15.wells ('A6')
-PBS = reservoir15.wells ('A7', 'A8', 'A9', 'A10', 'A11', 'A12')
+# Multiple wells of PBS needed; though if less that 48 oligos are used, only wells A7, A9 and A10 need to be filled
+PBS = reservoir15.wells ('A7', 'A8', 'A9', 'A10', 'A11', 'A12') 
 CRISPR_plasmid = tube2.wells ('A1')
 if electroporation == False:
     CaCL_1M = tube2.wells ('D6') 
@@ -61,12 +58,11 @@ def N_to_96(n): #Does not take inputs above 12
     else:
         raise NameError('N_to_96 input is above 12')
 
-#Add cells to each strip
+# Add cells to each strip
 p300.distribute(50, Bacteria, storage_oligos.columns()[0:12], touch_tip = False, new_tip = 'once')
 
-#Add CRISPR plasmid to each of the PCR strip containing different oligos
-for i in range(1):
-    p20.distribute(50/float(plasmid_conc[i]), CRISPR_plasmid, storage_oligos.columns()[0:12], touch_tip = True, new_tip = 'always', mix_after = (3, 15))
+# Add CRISPR plasmid to each of the PCR strip containing different oligos
+p20.transfer(50/float(plasmid_conc), CRISPR_plasmid, storage_oligos.columns()[0:12], touch_tip = True, new_tip = 'always', mix_after = (3, 15))
 
 # Heat shock protocol
 if electroporation == False:
@@ -76,54 +72,47 @@ if electroporation == False:
     p20.distribute(5, CaCL_1M, cold_plate.columns()[0:12], touch_tip=True, new_tip='once')
     
     # Moving to cold plate for 15 minute incubation at 4 degrees C
-    for i in range(1, math.ceil(oligos/8)+1):
+    for i in range(1, math.ceil(float(oligos)/8)+1):
         p300.pick_up_tip(tiprack_300[1][N_to_96(i)])
         p300.transfer(45, storage_oligos[N_to_96(i)], cold_plate[N_to_96(i)], touch_tip = True, new_tip = 'never', blow_out = True, mix_after = (2, 25))
         p300.return_tip(tiprack_300[1][N_to_96(i)])
     
-    protocol.delay(seconds = 15 * 60 - (math.ceil(oligos/8)*delay_per_column)) 
+    protocol.delay(seconds = 15 * 60 - (math.ceil(float(oligos)/8)*delay_per_column)) 
     
     # Moving to hot plate for heat shock at 42 degrees C
-    for i in range(1, math.ceil(oligos/8)+1):
+    for i in range(1, math.ceil(float(oligos)/8)+1):
         p300.pick_up_tip(tiprack_300[1][N_to_96(i)])
         p300.transfer(45, cold_plate[N_to_96(i)], hot_plate[N_to_96(i)], touch_tip = True, new_tip = 'never', blow_out = True)
         p300.return_tip(tiprack_300[1][N_to_96(i)])
         
-    # need to figure out how long the operation takes to subtract from this
-    protocol.delay(seconds = 90 - (math.ceil(oligos/8)*delay_per_column))  
+    protocol.delay(seconds = 90 - (math.ceil(float(oligos)/8)*delay_per_column))  
     
     # Moving to hot plate for 5 minute incubation at 4 degrees C
-    for i in range(1, math.ceil(oligos/8)+1):
+    for i in range(1, math.ceil(float(oligos)/8)+1):
         p300.pick_up_tip(tiprack_300[1][N_to_96(i)])
         p300.transfer(45, hot_plate[N_to_96(i)], cold_plate[N_to_96(i)], touch_tip = True, new_tip = 'never', blow_out = True)
         p300.return_tip(tiprack_300[1][N_to_96(i)])
 
     p300.pick_up_tip()
-    for i in range(1, math.ceil(oligos/8)+1):
+    for i in range(1, math.ceil(float(oligos)/8)+1):
         if i <= 6:
             p300.transfer(270, PBS[0], hot_plate[N_to_96(i)], touch_tip=False, new_tip='never')
         else:
             p300.transfer(270, PBS[1], hot_plate[N_to_96(i)], touch_tip=False, new_tip='never')
     p300.drop_tip()
     
-    temp_hot.set_temperature(growth_temp)
-    protocol.delay(seconds = 5 * 60 - (math.ceil(oligos/8)*delay_per_column)) 
+    temp_hot.set_temperature(float(growth_temp))
+    protocol.delay(seconds = 5 * 60 - (math.ceil(float(oligos)/8)*delay_per_column)) 
     
     # Moving to hot plate for 60 minute incubation at selected temperature
-    for i in range(1, math.ceil(oligos/8)+1):
+    for i in range(1, math.ceil(float(oligos)/8)+1):
         p300.pick_up_tip(tiprack_300[1][N_to_96(i)])
         p300.transfer(30, cold_plate[N_to_96(i)], hot_plate[N_to_96(i)], touch_tip = True, new_tip = 'never', blow_out = True, mix_after = (2, 150))
         p300.return_tip(tiprack_300[1][N_to_96(i)])
     
     temp_cold.deactivate()
-    protocol.delay(seconds = 60 * 60 - (math.ceil(oligos/8)*delay_per_column))
+    protocol.delay(seconds = 60 * 60 - (math.ceil(float(oligos)/8)*delay_per_column))
     temp_hot.deactivate()
-    
-    # Moving to distribution plate
-    # for i in range(1, math.ceil(oligos/8)+1):
-    #     p300.pick_up_tip(tiprack_300[1][N_to_96(i)])
-    #     p300.transfer(30, hot_plate[N_to_96(i)], dilution_plate_1[N_to_96(i)], touch_tip = True, trash = False, new_tip = 'never', blow_out = False, mix_after = (3, 15))
-    #     p300.return_tip(tiprack_300[1][N_to_96(i)])
 
 # Electroporation protocol
 elif electroporation == True:
--- opentrons_MAGE_Step2.py
+++ opentrons_MAGE_Step2.py
@@ -1,6 +1,6 @@
-#Import opentrons and run
-#import sys
-#!{sys.executable} -m pip install opentrons
+# import opentrons and run
+# import sys
+# !{sys.executable} -m pip install opentrons
 
 import math
 from opentrons import simulate
@@ -9,27 +9,16 @@ protocol = simulate.get_protocol_api('2.8')
 
 # Step 2
 protocol.pause('Replace tips and add agarose plates')
-#change modules 
-
-dilution_plate_1 = protocol.load_labware('corning_96_wellplate_360ul_flat', 1)   #1:10 Dilution from Heatshock Output
-temp_hot = protocol.load_module('tempdeck', 4)                #For Heatshock
+# change modules 
+dilution_plate_1 = protocol.load_labware('corning_96_wellplate_360ul_flat', 1)   # 1:10 Dilution from Heatshock Output
+dilution_plate_2 = protocol.load_labware('corning_96_wellplate_360ul_flat', 4)   # 1:100 Dilution from Heatshock Output
+reservoir15 = protocol.load_labware('nest_12_reservoir_15ml', 5)      # Bacterial Culture(A1), LB_Media(A6) and PBS
+temp_hot = protocol.load_module('tempdeck', 6)                # Heated plate from step 1 in module 6 remains unchanged
 hot_plate = temp_hot.load_labware('corning_96_wellplate_360ul_flat')
-reservoir15 = protocol.load_labware('nest_12_reservoir_15ml', 5)      #Bacterial Culture(A1), LB_Media(A6) and PBS
-solid_agar_glucose = protocol.load_labware('corning_96_wellplate_360ul_flat', 8)       #Solid Agar pre-made on the reservoir
-solid_agar_lupanine = protocol.load_labware('corning_96_wellplate_360ul_flat', 9)      #Solid Agar pre-made on the reservoir
-dilution_plate_2 = protocol.load_labware('corning_96_wellplate_360ul_flat', 11)   #1:100 Dilution from Heatshock Output
-#also hot_plate from step 1 in module 4 remains unchanged
+solid_agar_glucose = protocol.load_labware('corning_96_wellplate_360ul_flat', 10)       # Solid Agar pre-made on the reservoir
+solid_agar_lupanine = protocol.load_labware('corning_96_wellplate_360ul_flat', 11)      # Solid Agar pre-made on the reservoir
 
-# Variables
-oligos = 96
-electroporation = False
-
-#Reagents
-Bacteria = reservoir15.wells ('A1')
-Media = reservoir15.wells ('A6')
-PBS = reservoir15.wells ('A7', 'A8', 'A9', 'A10', 'A11', 'A12')
-
-#new pipette tips
+# add new pipette tips
 tiprack_300 = [
         protocol.load_labware(
             'opentrons_96_tiprack_300ul', str(s), '300ul Tips')
@@ -38,9 +27,19 @@ tiprack_300 = [
 tiprack_20 = [
         protocol.load_labware(
             'opentrons_96_filtertiprack_20ul', str(s), '300ul Tips')
-        for s in [3, 6]]
+        for s in [7, 8]]
 
-#pipettes
+
+# Variables
+oligos = input("Input number of mutagenic oligos to be used: ")
+electroporation = False
+
+# Reagents
+Bacteria = reservoir15.wells ('A1')
+Media = reservoir15.wells ('A6')
+PBS = reservoir15.wells ('A7', 'A8', 'A9', 'A10', 'A11', 'A12')
+
+# Pipettes
 p300 = protocol.load_instrument('p300_multi_gen2', 'right', tip_racks=tiprack_300)
 p20 = protocol.load_instrument('p20_multi_gen2', 'left', tip_racks=tiprack_20)
 protocol.max_speeds['Z'] = 10
@@ -53,9 +52,9 @@ def N_to_96(n): #Does not take inputs above
         raise NameError('N_to_96 input is above 12')
 
 
-#Add 270ul to dilution_plate_1 and dilution_plate_2 
+# Add 270ul to dilution_plate_1 and dilution_plate_2 
     p300.pick_up_tip()
-    for i in range(1, math.ceil(oligos/8)+1):
+    for i in range(1, math.ceil(float(oligos)/8)+1):
         if i <= 6:
             p300.transfer(270, PBS[2], dilution_plate_1[N_to_96(i)], touch_tip=False, new_tip='never')
             p300.transfer(270, PBS[3], dilution_plate_2[N_to_96(i)], touch_tip=False, new_tip='never')
@@ -64,19 +63,13 @@ def N_to_96(n): #Does not take inputs above
             p300.transfer(270, PBS[5], dilution_plate_2[N_to_96(i)], touch_tip=False, new_tip='never')
     p300.drop_tip()
     
-for i in range(1, math.ceil(oligos/8)+1):
+for i in range(1, math.ceil(float(oligos)/8)+1):
     p300.pick_up_tip()
     p300.transfer(30, hot_plate[N_to_96(i)], dilution_plate_1[N_to_96(i)], touch_tip = True, trash = False, new_tip = 'never', blow_out = True, mix_after = (3, 150))
     p300.transfer(30, dilution_plate_1[N_to_96(i)], dilution_plate_2[N_to_96(i)], touch_tip = True, trash = True, new_tip = 'never', blow_out = True, mix_after = (3, 150))
     p300.drop_tip()                                 #Only 1 tip used per transfer between plates per well
 
-#OUTPUT: in dilution_plate_2 in each well, we have bacterial cells in 1:100 dilution with different oligos
-
-
-#PLATING: Spot 10ul from dilution_plate_2 into solid_agar_glucose
-#PLATING: Spot 10ul from dilution_plate_2 into solid_agar_lupanine
 # Spoting constants:  
-
 spot_vol=10
 dead_vol=2
 spotting_dispense_rate=0.025
@@ -85,7 +78,7 @@ DISPENSING_HEIGHT = 5
 SAFE_HEIGHT = 15  # height avoids collision with agar tray.
 
 # Spot
-for i in range(1, math.ceil(oligos/8)+1):
+for i in range(1, math.ceil(float(oligos)/8)+1):
     p20.pick_up_tip()
     p20.aspirate(10 + dead_vol, dilution_plate_2[N_to_96(i)])
     p20.move_to(solid_agar_glucose[N_to_96(i)].top(SAFE_HEIGHT))
@@ -106,7 +99,7 @@ for i in range(1, math.ceil(oligos/8)+1):
     p20.drop_tip()
     
 
-for i in range(1, math.ceil(oligos/8)+1):
+for i in range(1, math.ceil(float(oligos)/8)+1):
     p20.pick_up_tip()
     p20.aspirate(10 + dead_vol, dilution_plate_2[N_to_96(i)])
     p20.move_to(solid_agar_lupanine[N_to_96(i)].top(SAFE_HEIGHT))
@@ -126,8 +119,6 @@ for i in range(1, math.ceil(oligos/8)+1):
     # p20.dispense(dead_vol, spotting_waste) 
     # p20.blow_out()
     p20.drop_tip()
-
-# Trial desktop
 
 for line in protocol.commands(): 
         print(line)
